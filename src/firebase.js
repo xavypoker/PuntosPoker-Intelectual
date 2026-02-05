f861cecdd05924ddccbbce8e7a4ddc10309b29ad
xavypoker <xavy1106@gmail.com>
2026-02-05 15:32:38 -0600

    Update firebase.js
    Fix database export

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBMVItjsBOCIEaT2v_xzuKBim_tAdARbU",
   authDomain: "poker-intelectual.firebaseapp.com",
@@ -17,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-XJ52LR2ZGR"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+
 export { database };
